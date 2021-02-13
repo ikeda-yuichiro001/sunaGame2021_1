2959732043a9ac030564a558419c10d34bd53c2f
snbkr3 <snbkr3kumakou@gmail.com>
2021-02-13 23:28:41 +0900

    sss

---
+++ sunaGame000/sunaGame2021_1/Assets/Script/status.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class status : MonoBehaviour
+{
+
+    public int HP;
+    public int Spst;
+
+    void Start()
+    {
+        //HPの最大値を100に
+        //HPの最小値を0に
+        //
+        HP = 1;　//HPに1を代入
+        
+    }
+
+    void Update()
+    {
+        //HPが0以下になるとゲームオーバー
+        //HPが80～100の時は健康
+        //HPが50～80の時は少し不安
+        //HP
+        
+    }
+}
