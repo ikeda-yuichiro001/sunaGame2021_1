7bea374d257d98fd70299bf1ee2f6cd9d927e5ae
YuichiroIKEDA <ikeda.yuichiro.w.00@gmail.com>
2021-02-13 23:55:54 +0900

    ゲームオーバーの画面とかのシステム

---
+++ sunaGame000/sunaGame2021_1/Assets/GameOver/GameOver.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : UnityEngine.MonoBehaviour
+{ 
+    void Start() => Invoke("NEXT", 3.5f);
+    void NEXT() => SceneLoader.Load("Logo");
+}
