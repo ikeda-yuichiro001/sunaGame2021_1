25396f145bbb2aec0ba05800bb72b527474caeb2
YuichiroIKEDA <ikeda.yuichiro.w.00@gmail.com>
2021-03-13 23:31:21 +0900

    アニメーションクリップ(待機,歩き,ジャンプ) 移動、ジャンプ、落下ダメージ処理

---
+++ sunaGame000/sunaGame2021_1/Assets/_Player/Animations/clip/exam.cs
@@ -0,0 +1,28 @@
+﻿
+
+public class LIST
+{
+    public OKASIs omoti; 
+
+    public void Start()
+    {
+        omoti = new OKASIs();//初期化する
+        omoti.count = 100;
+        omoti.okasi_Data = new OKASI();
+        omoti.okasi_Data.name = "おもち";
+        omoti.okasi_Data.heal = 100; 
+    }
+
+}
+
+public class OKASIs
+{
+    public OKASI okasi_Data;
+    public int count;
+}
+
+public class OKASI
+{
+    public string name;
+    public int heal;
+}
