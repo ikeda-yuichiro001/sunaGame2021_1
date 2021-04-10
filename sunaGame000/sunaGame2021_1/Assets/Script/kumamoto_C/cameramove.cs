d33f2e0c7069f684445eaff9516f3f2ae985d900
snbkr3 <snbkr3kumakou@gmail.com>
2021-04-10 23:15:36 +0900

    タグでitemが入っているオブジェクトとぶつかったらそのアイテムのコンポーネントを取得

---
+++ sunaGame000/sunaGame2021_1/Assets/Script/kumamoto_C/cameramove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cameramove : MonoBehaviour
+{
+
+    public int left, light, center, back;
+
+    void Start()
+    {
+        left = 1;
+        light = 1;
+        center = 1;
+        back = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene("test_kumamoto");
+        }
+
+
+        // 左に移動
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(-left, 0.0f, 0.0f);
+        }
+        // 右に移動
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(light, 0.0f, 0.0f);
+        }
+        // 前に移動
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(0.0f, 0.0f, center);
+        }
+        // 後ろに移動
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(0.0f, 0.0f, -back);
+        }
+    }
+}
---
+++ sunaGame000/sunaGame2021_1/Assets/Script/kumamoto_C/itemGET.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//動かすキャラクターにrigidbodyをつけてこいつをつける
+//アイテムにはtagにItemを設定する
+//tagにItemがついているオブジェクトに衝突するとコンポーネントを取得する
+
+public class itemGET : MonoBehaviour
+{
+    public ItemStatus itemstatus;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Item")
+        {
+            itemstatus = transform.root.gameObject.GetComponent<ItemStatus>();
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
